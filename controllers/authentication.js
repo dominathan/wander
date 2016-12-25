f85c7edfbe918dd7b630acaeec39886e63d0fbd2
Nathan Hall <nathan.mh@gmail.com>
2016-12-24 22:50:00 -0600

    hardcode grpah url

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -14,8 +14,9 @@ router.route('/auth/facebook/callback')
   .post(function(req, res, next) {
     // console.log("REDIRECT: ", authConfig.facebookAuth.callbackUrl)
     // var accessTokenUrl = 'https://graph.facebook.com/oauth/access_token';
-    var graphApiUrl = 'https://graph.facebook.com/me';
     var accessToken = req.body.accessToken
+    var graphApiUrl = 'https://graph.facebook.com/me?access_token=' + access_token;
+
     console.log("THIS IS ACCESS TOKEN", accessToken)
 
     // // Step 1. Exchange authorization code for access token.
@@ -25,7 +26,7 @@ router.route('/auth/facebook/callback')
     //   }
     //   accessToken = qs.parse(accessToken);
     //   // Step 2. Retrieve profile information about the current user.
-      request.get({ url: graphApiUrl, qs: accessToken, json: true }, function(err, response, profile) {
+      request.get(graphApiUrl, function(err, response, profile) {
         if (response.statusCode !== 200) return res.status(500).send({ message: profile.error.message });
 
         console.log('PROFILE INFO', profile);
